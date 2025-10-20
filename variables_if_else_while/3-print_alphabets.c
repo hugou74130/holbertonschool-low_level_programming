379952a7850e514588345ec26f90e93748c6fd94
Hugo <hugoramos1466@gmail.com>
2025-10-20 15:57:41 +0200

    3print_alphabet.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si succès
+ */
+
+int main(void)
+{
+char c;
+char b;
+for (c = 'A'; c <= 'Z'; c++)
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar(\n);
+
+putchar(b);
+
+return (0);
+}
