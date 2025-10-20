d29c1f6b8b631b53d1070f02aef3d3c9178f61c9
Hugo <hugoramos1466@gmail.com>
2025-10-20 16:25:56 +0200

    4print_number.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si succès
+ */
+
+int main(void)
+{
+int c;
+
+for (c = '0'; c <= '10'; c++)
+putchar(c);
+return (0);
+}
