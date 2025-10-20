d870c8dd19d76448f7387a8066e6faa60dd442da
Hugo <hugoramos1466@gmail.com>
2025-10-20 15:31:55 +0200

    print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si succès
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
