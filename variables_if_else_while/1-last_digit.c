efa91e4de40985a1e572930338a7bcce0681edc6
Hugo <hugoramos1466@gmail.com>
2025-10-20 13:42:13 +0200

    last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ n = rand() - RAND_MAX / 2; *
 last_digit = n % 10;
 if (n > 5)
 {
-printf("and is greater than 5\n", n);
+printf("%d and is greater than 5\n", n);
 }
 else if (n == 0)
 {
-printf("and is 0\n", n);
+printf("%d and is 0\n", n);
 }
 else
 {
-printf("and is less than 6 and not 0\n", n);
+printf("%d and is less than 6 and not 0\n", n);
 }
 return (0);
 }
