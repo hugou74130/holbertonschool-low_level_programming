ad494acbba25904a98c8243dedc0725999fc487c
Hugo <hugoramos1466@gmail.com>
2025-10-20 16:36:02 +0200

    6print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si succès
+ */
+
+int main(void)
+{
+int c;
+
+for (n = 0; n < 10; c++)
+putchar(n + '0');
+putchar('\n');
+return (0);
+}
