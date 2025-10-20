f1a27229456638a09f27d3828b568ee9298c1b47
Hugo <hugoramos1466@gmail.com>
2025-10-20 10:32:30 +0200

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-
+/**
+ * main - point d'entrée du programme
+ *
+ * Return: 0 si succès
+ */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
