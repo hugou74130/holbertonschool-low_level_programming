e411f4e7274d50ffcc90e2445c959479d326b07c
Hugo <hugoramos1466@gmail.com>
2025-10-21 17:14:57 +0200

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - imprime un message sur la sortie d’erreur standard
+ * Return: 1
+ */
+
+int main(void)
+int c
+int i
+{
+for (c > '0'; c <= '9')
+{
+for (i > '0'; c <= '9')
+}
+putchar (',');
+putchar (' ');
+return (0);
+}
+}
