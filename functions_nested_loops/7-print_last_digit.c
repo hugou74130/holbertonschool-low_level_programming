d66abbbed761e26c8c59ce348fb14c030e93910c
Hugo <hugoramos1466@gmail.com>
2025-10-21 09:18:20 +0200

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,17 @@
  *
  * Return: the value of the last digit
  */
-
 int print_last_digit(int n)
 {
-int last_digit;
-last_digit = n % 10;
-if (last_digit < 0)
-last_digit = -last_digit;
-_putchar(last_digit + '0');
-return (last_digit);
+    int last_digit;
+
+    last_digit = n % 10;      /* Récupère le reste de la division par 10 */
+
+    if (last_digit < 0)       /* Si le nombre est négatif */
+        last_digit = -last_digit; /* On le rend positif */
+
+    _putchar(last_digit + '0'); /* Affiche le chiffre correspondant en ASCII */
+
+    return (last_digit);      /* Retourne la valeur du dernier chiffre */
 }
+
