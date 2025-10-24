be94ce40c99d2f73b40bd06609069c4af4ba3b25
Hugo <hugoramos1466@gmail.com>
2025-10-24 10:21:18 +0200

    100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -26,15 +26,4 @@ printf(",%4d", result);
 printf("\n");
 }
 }
-int main (void)
-{
-print_times_table(3);
-putchar('\n');
-print_times_table(5);
-putchar('\n');
-print_times_table(98);
-putchar('\n');
-print_times_table(12);  
-return (0);
-}
 
