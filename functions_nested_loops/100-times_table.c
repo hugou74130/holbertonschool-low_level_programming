27cd14426514c6d937e91e031bac538bb3cb20a6
Hugo <hugoramos1466@gmail.com>
2025-10-24 16:25:25 +0200

    test

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,46 +8,46 @@
 
 void print_number(int n)
 {
-    if (n / 10)
-        print_number(n / 10);
-    _putchar((n % 10) + '0');
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
 }
 
 void print_times_table(int n)
 {
-    int i, j, product;
+int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+if (n < 0 || n > 15)
+return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+product = i * j;
 
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+if (j == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
 
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                else if (product < 100)
-                {
-                    _putchar(' ');
-                }
+if (product < 10)
+{
+_putchar(' ');
+_putchar(' ');
+}
+else if (product < 100)
+{
+_putchar(' ');
+}
 
-                print_number(product);
-            }
-        }
-        _putchar('\n');
-    }
+print_number(product);
+}
+}
+_putchar('\n');
+}
 }
