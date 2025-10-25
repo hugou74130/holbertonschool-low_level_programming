21ddff0a53d2f5a3d7a0a574e59174f518566e85
Hugo <hugoramos1466@gmail.com>
2025-10-25 14:20:12 +0200

    104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -5,39 +5,45 @@
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
 int count;
-unsigned long a = 1, b = 2, a1, a2, b1, b2, carry;
+unsigned long a = 1, b = 2, a1, a2, b1, b2, t1, t2;
 unsigned long limit = 1000000000;
 
 printf("%lu, %lu", a, b);
 
 for (count = 3; count <= 98; count++)
 {
-if (b + a < limit)
+if (b < limit)
 {
-b = b + a;
-a = b - a;
+t1 = a;
+a = b;
+b = t1 + b;
 printf(", %lu", b);
 }
 else
 {
-
 a1 = a / limit;
 a2 = a % limit;
 b1 = b / limit;
 b2 = b % limit;
 
-carry = (a2 + b2) / limit;
-b1 = a1 + b1 + carry;
-b2 = (a2 + b2) % limit;
-
-a = b;
+t1 = a1;
+t2 = a2;
 a1 = b1;
 a2 = b2;
 
- printf(", %lu%09lu", b1, b2);
+b1 = t1 + b1;
+b2 = t2 + b2;
+if (b2 >= limit)
+{
+b1 += 1;
+b2 -= limit;
+}
+
+printf(", %lu%09lu", b1, b2);
 }
 }
 
