fdf8f221b17f7a38094b4f7588c32d31ba79e3c8
Hugo <hugoramos1466@gmail.com>
2025-10-27 16:52:30 +0100

    Refactor puts2 function documentation to clarify purpose and parameters

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * rev_string - reverses a string
- * @s: the string to be reversed
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: the string to be printed
  */
-
 void puts2(char *str)
 {
 	int len = 0, i;
 
-	/* Find the length of the string */
 	while (str[len] != '\0')
 		len++;
-	/* Print every second character */
+
 	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
