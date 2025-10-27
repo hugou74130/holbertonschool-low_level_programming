a4f8c9b23b33a1c0747d5281bc559e30542c0aa6
Hugo <hugoramos1466@gmail.com>
2025-10-27 09:51:41 +0100

    Add function prototype for swap_int in main.h

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+
+	char *str;
+
+	_strlen(s);
+}
