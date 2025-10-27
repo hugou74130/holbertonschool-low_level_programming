5d2fe7475f9008cb3f04231a53a27a90b6c741f8
Hugo <hugoramos1466@gmail.com>
2025-10-27 16:30:20 +0100

    Add puts2 function to print every second character of a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ */
+void puts2(char *str)
+{
+	int len = 0, i;
+
+	/* Find the length of the string */
+	while (str[len] != '\0')
+		;
+	len++;
+	/* Print every second character */
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
