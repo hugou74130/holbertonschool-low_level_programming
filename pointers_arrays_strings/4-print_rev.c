db5de225f2fd344899909d888c4cf93dd53a07de
Hugo <hugoramos1466@gmail.com>
2025-10-27 14:17:16 +0100

    Fix variable shadowing in print_rev function

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
 
 void print_rev(char *s)
 {
-	char *str;
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	char *s;
+	s = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(s);
 }
