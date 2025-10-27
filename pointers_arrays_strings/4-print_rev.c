70d71c5e15cc821d782f7f64b1496244e84d3285
Hugo <hugoramos1466@gmail.com>
2025-10-27 14:29:49 +0100

    Refactor print_rev function to improve string reversal logic and enhance readability

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse
- * @s: pointer to the string to be printed
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to print
  */
 void print_rev(char *s)
 {
 	int len = 0;
 
+	/* Find the length of the string */
 	while (s[len] != '\0')
 		len++;
 
-	for (int i = len - 1; i >= 0; i--)
-		putchar(s[i]);
+	/* Print characters in reverse */
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
 
-	putchar('\n');
+	_putchar('\n');
 }
