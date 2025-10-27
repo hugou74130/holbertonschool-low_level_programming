77534c515a3aaf8e7207879d56936efc9d6c4b97
Hugo <hugoramos1466@gmail.com>
2025-10-27 15:15:55 +0100

    Add string reversal function and test program
    - Implemented the rev_string function to reverse a given string in place.
    - Created a test program to demonstrate the usage of the rev_string function.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string to be reversed
+ */
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char temp;
+	/* Find the length of the string */
+	while (s[len] != '\0')
+		len++;
+	/* Swap characters from start and end moving towards the center */
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char *p = "salut, monde\n";
+	printf("%s", p);
+	return (0);
+}
