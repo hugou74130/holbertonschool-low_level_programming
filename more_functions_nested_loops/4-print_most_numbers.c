b7df2860aab9e911fdcd523916c1e386952b1cb0
Hugo <hugoramos1466@gmail.com>
2025-10-27 14:15:21 +0100

    Add print_rev function to print a string in reverse

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,12 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints the numbers from 0 to 9
-*                      except 2 and 4, followed by a new line
-*
-* You can only use _putchar twice
-*/
+ * print_most_numbers - prints the numbers from 0 to 9
+ *                      except 2 and 4, followed by a new line
+ *
+ * You can only use _putchar twice
+ */
 
-void print_most_numbers(void)
+void print_rev(char *s)
 {
-char c = '0';
-while (c <= '9')
-{
-if (c != '2' && c != '4')
-_putchar(c);
-c++;
-}
-_putchar('\n');
 }
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string to be printed
+ */
+
+void print_rev(char *s)
+{
+	char *str;
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
