81629dc9c16ab4e1d02bb806fbae9fe0e11d711d
Hugo <hugoramos1466@gmail.com>
2025-10-28 15:55:35 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,6 +7,7 @@ int _atoi(char *s)
 	int i = 0;
 	int signe = 1;
 	int nb = 0;
+
 	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
@@ -15,18 +16,22 @@ int _atoi(char *s)
 	}
 	if (s[i] < '0' || s[i] > '9')
 		return (0);
+
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		nb = nb * 10 + (s[i] - '0');
-		++i;
+		int digit = s[i] - '0';
+		if (signe == 1)
+		{
+			if (nb > (INT_MAX - digit) / 10)
+				return INT_MAX;
+		}
+		else
+		{
+			if (nb > (-(INT_MIN + digit)) / 10)
+				return INT_MIN;
+			nb = nb * 10 + digit;
+			i++;
+		}
+
+		return nb * signe;
 	}
-
-	return nb * signe;
-
-	if (nb > INT_MAX)
-		return INT_MAX;
-	if (nb < INT_MIN)
-		return INT_MIN;
-
-	return (int)nb;
-}
