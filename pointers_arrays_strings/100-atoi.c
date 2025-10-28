342699bc7aa8716c414c811b8dab3119da4576d8
Hugo <hugoramos1466@gmail.com>
2025-10-28 14:50:45 +0100

    Refactor _atoi function to correct logic for parsing integers from string

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,21 @@
 
 int _atoi(char *s)
 {
-	char str[] = ("");
-	s = _atoi(str);
-	printf("test %d", s);
+	int i = 0;
+	int signe = 1;
+	int nb = 0;
+	while (s[i] && (s[i] > '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			signe *= -1;
+		i++;
+	}
+	if (s[i] < '0' || s[i] > '9')
+		return (0);
+	while (s[i] < '0' && s[i] > '9')
+		;
+	{
+		nb = nb * 10 + (s[i] - '0');
+	}
+	return nb * signe;
 }
