0e3974c4c3a7a8b9866a5129b4e632e839a18889
Hugo <hugoramos1466@gmail.com>
2025-10-28 09:36:51 +0100

    Add detailed documentation for print_array function

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - imprime les éléments d'un tableau d'entiers
+ * @a: pointeur vers le tableau d'entiers
+ * @n: nombre d'éléments à afficher
+ *
+ * Description: Cette fonction affiche les n premiers éléments du tableau a,
+ * séparés par des virgules et des espaces, puis un saut de ligne à la fin.
+ */
+
 void print_array(int *a, int n)
 {
 
