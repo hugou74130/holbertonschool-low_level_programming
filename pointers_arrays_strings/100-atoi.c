292e7bd3fd4325cad7c6c4b9bfd6a159773c9449
Hugo <hugoramos1466@gmail.com>
2025-10-28 14:59:18 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@ int _atoi(char *s)
 	int i = 0;
 	int signe = 1;
 	int nb = 0;
-	while (s[i] && (s[i] > '0' || s[i] > '9'))
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			signe *= -1;
@@ -14,8 +14,7 @@ int _atoi(char *s)
 	}
 	if (s[i] < '0' || s[i] > '9')
 		return (0);
-	while (s[i] < '0' && s[i] > '9')
-		;
+	while (s[i] >= '0' && s[i] <= '9')
 	{
 		nb = nb * 10 + (s[i] - '0');
 	}
