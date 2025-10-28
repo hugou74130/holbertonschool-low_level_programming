fa5059f8e3764da785ed56f7d705ebd570ef1c17
Hugo <hugoramos1466@gmail.com>
2025-10-28 16:16:54 +0100

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 #include <limits.h>
 
 int _atoi(char *s)
 {
 	int i = 0;
 	int signe = 1;
-	int nb = 0;
+	unsigned int nb = 0;
 
 	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
@@ -19,18 +18,25 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if (signe == 1)
+		/* Vérification d'overflow avant la multiplication */
+		if (nb > (unsigned int)INT_MAX / 10)
 		{
-			if (nb > (INT_MAX - (s[i] - '0')) / 10)
-				return INT_MAX;
+			return (signe == 1) ? INT_MAX : INT_MIN;
 		}
-		else
-		{
-			if (nb > (-(INT_MIN + (s[i] - '0'))) / 10)
-				return INT_MIN;
-		}
+
 		nb = nb * 10 + (s[i] - '0');
+
+		/* Vérification après l'addition */
+		if (signe == 1 && nb > (unsigned int)INT_MAX)
+			return INT_MAX;
+		if (signe == -1 && nb > (unsigned int)INT_MAX + 1)
+			return INT_MIN;
+
 		i++;
 	}
-	return nb * signe;
+
+	/* Conversion finale sécurisée */
+	if (signe == -1)
+		return -(int)nb;
+	return (int)nb;
 }
