5209e798552f6c6c9557a9afb921b88e751475b8
Hugo <hugoramos1466@gmail.com>
2025-10-28 12:07:14 +0100

    Fix _strcpy function to correctly copy strings and remove debug print statement

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strcpy - copies string from src to dest
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to dest
+ */
 
 char *_strcpy(char *dest, char *src)
 {
-	printf("%d %d", *dest, *src);
+	int i = 0;
+	while (src[i] != '\0')
+		dest[i] = src[i];
+	i++;
+	dest[i] = '\0';
+	return dest;
 }
