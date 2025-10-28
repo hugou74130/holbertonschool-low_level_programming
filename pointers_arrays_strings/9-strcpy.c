8988437a6e73d91a03c5e8d943472d6b763e57e5
Hugo <hugoramos1466@gmail.com>
2025-10-28 12:25:14 +0100

    Format return statement in _strcpy function for consistency

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _strcpy - copies string from src to dest
  * @dest: destination string
  * @src: source string
  * Return: pointer to dest
  */
-
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
