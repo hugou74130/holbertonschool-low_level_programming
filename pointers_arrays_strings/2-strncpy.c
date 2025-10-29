4fd2a5ce0ea3ef72c5077602661bca5d2bbe394d
Hugo <hugoramos1466@gmail.com>
2025-10-29 10:10:53 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * ma_fonction_exemple - Décrit brièvement le but de la fonction.
+ * @a: Description du paramètre 'a' (par exemple : le premier entier).
+ * @b: Description du paramètre 'b' (par exemple : le second entier).
+ *
+ * Return: Description de la valeur de retour (par exemple : la somme de a et b).
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
