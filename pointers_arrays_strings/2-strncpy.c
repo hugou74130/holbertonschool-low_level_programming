ea3729a6a68616b2062e5bcfeef820765c0f3f41
Hugo <hugoramos1466@gmail.com>
2025-10-29 10:04:04 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,15 +3,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		while (i < n && src[i] != '\0')
-
-			i++;
+		dest[i] = src[i];
+		i++;
 	}
 	while (i < n)
-		dest[i] = '\0';
-	i++;
 	{
-		return (dest);
+		dest[i] = '\0';
+		i++;
 	}
+	return (dest);
 }
