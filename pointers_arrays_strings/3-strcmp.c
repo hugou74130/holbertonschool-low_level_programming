2578e05584d613fbda766bac5a8ec7f4ebd1d425
Hugo <hugoramos1466@gmail.com>
2025-10-29 10:49:19 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * _strcmp - Compare deux chaînes de caractères (s1 et s2)
+ * lexicographiquement (ordre alphabétique).
+ * @s1: La première chaîne à comparer.
+ * @si2: La deuxième chaîne à comparer.
+ *
+ * Return: Un entier indiquant la relation entre les chaînes:
+ * - 0 si s1 est égal à si2.
+ * - Une valeur négative si s1 est inférieure à si2.
+ * - Une valeur positive si s1 est supérieure à si2.
+ */
+
 int _strcmp(char *s1, char *si2)
 {
 	int i = 0;
