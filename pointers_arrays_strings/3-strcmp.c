c000ea9631502207c5f18c4622c18eb36e9b0ef5
Hugo <hugoramos1466@gmail.com>
2025-10-29 10:47:35 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,10 @@ int _strcmp(char *s1, char *si2)
 	while (s1[i] != '\0' && si2[i] != '\0')
 	{
 		if (s1[i] != si2[i])
-			i++;
+		{
+			return (s1[i] - si2[i]);
+		}
+		i++;
 	}
 	return (s1[i] - si2[i]);
 }
