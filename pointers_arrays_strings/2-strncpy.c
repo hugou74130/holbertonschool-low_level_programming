40d9f39a3275a203d1ad289a4de338be7fc41793
Hugo <hugoramos1466@gmail.com>
2025-10-29 10:12:08 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,30 @@
 #include "main.h"
-/**
- * ma_fonction_exemple - Décrit brièvement le but de la fonction.
- * @a: Description du paramètre 'a' (par exemple : le premier entier).
- * @b: Description du paramètre 'b' (par exemple : le second entier).
- *
- * Return: Description de la valeur de retour (par exemple : la somme de a et b).
- */
 
+/**
+ * _strncpy - Copie au maximum n octets de la chaîne source vers la destination.
+ * @dest: Le pointeur vers le tampon de destination.
+ * @src: Le pointeur vers la chaîne source.
+ * @n: Le nombre maximal d'octets à copier.
+ *
+ * Return: Le pointeur vers la chaîne de destination dest.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+
+	/* Betty style requires a blank line after declarations */
+
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
