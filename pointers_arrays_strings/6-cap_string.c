6735c2d7f51cb81e513213dcc95d3693e3090a10
Hugo <hugoramos1466@gmail.com>
2025-10-29 15:20:41 +0100

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,43 @@
 #include "main.h"
 
+/**
+ * cap_string - Met en majuscule la première lettre de chaque mot d'une chaîne.
+ * @s: Pointeur vers la chaîne de caractères à modifier.
+ *
+ * Cette fonction parcourt la chaîne et convertit en majuscule
+ * chaque lettre qui suit un séparateur de mots.
+ * Les séparateurs considérés sont : espace, tabulation, saut de ligne,
+ * virgule, point-virgule, point, point d’exclamation, point d’interrogation,
+ * guillemets, parenthèses et accolades.
+ *
+ * Return: Pointeur vers la chaîne modifiée.
+ */
+
 char *cap_string(char *s)
 {
 	int i = 0;
 	int new_word = 1;
+
 	while (s[i] != '\0')
-		;
-	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-		s[i] == ',' || s[i] == ';' || s[i] == '.' ||
-		s[i] == '!' || s[i] == '?' || s[i] == '"' ||
-		s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 	{
-		new_word = 1; // indique que le prochain caractère doit être majusculé
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+			s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+			s[i] == '!' || s[i] == '?' || s[i] == '"' ||
+			s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			new_word = 1;
+		}
+		else if (new_word == 1 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+			new_word = 0;
+		}
+		else
+		{
+			new_word = 0;
+		}
+		i++;
 	}
-	else if (new_word == 1 && s[i] >= 'a' && s[i] <= 'z')
-	{
-		s[i] = s[i] - 32; // ici on met en majuscule
-		new_word = 0;
-	}
-	else
-	{
-		new_word = 0; // caractère normal
-	}
+
+	return (s);
 }
