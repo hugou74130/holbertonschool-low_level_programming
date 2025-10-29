7ef3f36a5ed32e359a3b7f50eb91bda238c72b86
Hugo <hugoramos1466@gmail.com>
2025-10-29 13:37:33 +0100

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * reverse_array - Inverse le contenu d'un tableau d'entiers
+ * @a: Pointeur vers le tableau d'entiers
+ * @n: Le nombre d'éléments dans le tableau
+ *
+ * Description: Cette fonction échange les éléments de manière symétrique
+ * (premier avec dernier, deuxième avec avant-dernier, etc.) jusqu'à ce
+ * que le milieu du tableau soit atteint. La modification est faite
+ * directement (in-place) sur le tableau pointé par 'a'.
+ *
+ * Return: void (Aucun retour)
+ */
 void reverse_array(int *a, int n)
 {
 	int temp;
