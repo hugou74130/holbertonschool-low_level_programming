cde0da4c2a57dd6fafc2c4bc402d1b4b0744a552
Hugo <hugoramos1466@gmail.com>
2025-10-29 15:45:48 +0100

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -4,38 +4,29 @@
  * leet - Encode une chaîne de caractères en “leet” (1337).
  * @s: Pointeur vers la chaîne à encoder.
  *
- * Cette fonction parcourt la chaîne de caractères et remplace certaines
- * lettres par des chiffres correspondants selon le style “leet” :
- *   - 'a' et 'A' deviennent '4'
- *   - 'e' et 'E' deviennent '3'
- *   - 'o' et 'O' deviennent '0'
- *   - 't' et 'T' deviennent '7'
- *   - 'l' et 'L' deviennent '1'
- *
- * Seuls ces caractères sont remplacés ; tous les autres restent inchangés.
- * La chaîne est modifiée in-place et la fonction retourne un pointeur vers elle.
+ * Cette fonction parcourt la chaîne et remplace certaines lettres
+ * par des chiffres selon le style “leet”.
  *
  * Return: Pointeur vers la chaîne modifiée.
  */
 
 char *leet(char *s)
 {
-	int i = 0;
-	int j;
+	int i = 0, j;
 	char letter[] = "aAeEoOtTlL";
 	char number[] = "4433007711";
+
+	while (s[i] != '\0')
 	{
-		while (s[i] != '\0')
-		{
-			i++;
-		}
+		j = 0;
 		while (letter[j] != '\0')
-			if (s[i] == letter[j])
-				s[i] == number[j];
 		{
+			if (s[i] == letter[j])
+				s[i] = number[j];
 			j++;
 		}
 		i++;
 	}
-	return (s);
+
+	return s;
 }
