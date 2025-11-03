f3992b3f309456afa383a04459ab7c6890d55f7f
Hugo <hugoramos1466@gmail.com>
2025-11-03 07:39:29 +0100

    memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * _memset - Remplit la mémoire avec une constante donnée.
+ * @s: Pointeur vers la zone mémoire à remplir.
+ * @b: Valeur constante à copier dans la mémoire (interprétée comme un char).
+ * @n: Nombre d’octets de la mémoire à remplir.
+ *
+ * Description: Cette fonction remplit les `n` premiers octets
+ * de la zone mémoire pointée par `s` avec la valeur constante `b`.
+ *
+ * Return: Un pointeur vers la zone mémoire `s`.
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 
 {
