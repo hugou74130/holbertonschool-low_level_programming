de9d5ef172b1de8b58424124da85658ca5c78192
Hugo <hugoramos1466@gmail.com>
2025-11-03 15:31:16 +0100

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: pointer to the beginning of the matrix (int array)
+ * @size: size of the square matrix
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+
+{
+	int i;
+	int j;
+	int sum1;
+	int sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				sum1 += a[i * size + j];
+			if (i + j == size - 1)
+				sum2 += a[i * size + j];
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -48,4 +48,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif /* MAIN_H */
