b814532228729b52f323f0bc8469052f0dcbbc49
Hugo <hugoramos1466@gmail.com>
2025-11-04 07:30:36 +0100

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,11 +4,10 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-
-		_putchar('\n');
-	return;
 	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
+		_putchar('\n');
+		return;
 	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
