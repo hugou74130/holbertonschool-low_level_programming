0c1844020201c55460bc8609a42a46b9f8de8f8c
Hugo <hugoramos1466@gmail.com>
2025-11-04 12:34:40 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,16 +8,13 @@
  *
  * Return: The natural square root if it exists, -1 otherwise
  */
-
 int _sqrt_recursion(int n, int i)
-
 {
-
 	if (n < 0)
-		return (-1);
-	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
+	if (i * i > n)
+		return (-1);
 	return (_sqrt_recursion(n, i + 1));
 }
