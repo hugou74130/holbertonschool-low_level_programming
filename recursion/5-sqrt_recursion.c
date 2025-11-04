3660d7307c94e9199028d704e5969a62baacbc55
Hugo <hugoramos1466@gmail.com>
2025-11-04 12:27:49 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -16,8 +16,8 @@ int _sqrt_recursion(int n, int i)
 	if (n < '\0')
 		return (-1);
 	if (i * i > n)
-		return (1);
+		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt_recursion(i, n + 1));
+	return (_sqrt_recursion(n, i + 1));
 }
