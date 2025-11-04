9743ca4e902f9ec34f36e0f9edeabca0ea2be9e7
Hugo <hugoramos1466@gmail.com>
2025-11-04 15:31:40 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,11 +6,9 @@ int main(int argc, char *argv[])
 	int sum1;
 	int sum2;
 	int result;
-	{
-		if (argc != 3)
-			printf("Error");
-		return (1);
-	}
+	if (argc != 3)
+		printf("Error\n");
+	return (1);
 	sum1 = atoi(argv[1]);
 	sum2 = atoi(argv[2]);
 	result = sum1 * sum2;
