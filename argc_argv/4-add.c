3bc98331de4f8ab0addae2fac4e373119aa3c490
Hugo <hugoramos1466@gmail.com>
2025-11-04 16:21:14 +0100

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Description: Adds all positive numbers passed as command line arguments.
+ * If no arguments, prints 0. If any argument contains non-digit characters,
+ * prints "Error" and returns 1.
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+	int j;
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	sum = 0;
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!_isdigit(argv[i][j]))
+				printf("Error\n");
+			return (1);
+		}
+		j++;
+	}
+	sum += _atoi(argv[i]);
+	i++;
+	{
+		printf("%d", sum);
+	}
+	return (0);
+}
