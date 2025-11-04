8c0e94f73708421292ae0229ddcd677249eb4cc2
Hugo <hugoramos1466@gmail.com>
2025-11-04 07:12:02 +0100

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _puts_recursion - Affiche une chaîne de caractères suivie d'une nouvelle ligne
+ * @s: Pointeur sur la chaîne de caractères à afficher
+ *
+ * Description: Fonction récursive qui affiche chaque caractère d'une chaîne
+ * jusqu'à rencontrer le caractère nul ('\0'). Après avoir affiché tous les
+ * caractères, la fonction affiche une nouvelle ligne.
+ *
+ * Return: Aucune valeur de retour (void)
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
