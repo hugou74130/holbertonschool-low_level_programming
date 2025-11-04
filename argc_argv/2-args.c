3db426e117a0aa1fdbea72af26da3ad3aa912e1e
Hugo <hugoramos1466@gmail.com>
2025-11-04 15:00:39 +0100

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - prints all arguments passed into the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Description: This program prints each command-line argument
+ * on a separate line, including the program name.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	int i;
+	while (i < argc)
+
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
