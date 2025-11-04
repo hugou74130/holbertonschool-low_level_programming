2a016d2a7e6e64403d5fbdb56fe2df1b6a82c8dc
Hugo <hugoramos1466@gmail.com>
2025-11-04 15:36:48 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Description: Multiplies two integer numbers passed as command line arguments
+ * and prints the result. If the number of arguments is incorrect, prints "Error"
+ * and returns 1.
+ *
+ * Return: 0 on success, 1 on error
+ */
+
 int main(int argc, char *argv[])
 {
 	int sum1;
 	int sum2;
 	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
