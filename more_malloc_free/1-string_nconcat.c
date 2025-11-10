9e1e5451ea8fefeb9c2563df77bfb7ee480170f5
Hugo <hugoramos1466@gmail.com>
2025-11-10 22:15:46 +0100

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -19,47 +19,38 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
-	unsigned int len2, len1, total_len;
-	char *result;
-	unsigned int i;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
-	if (n >= len2)
-		total_len = len1 + len2;
-	else
-		total_len = len1 + n;
-
-	result = malloc(sizeof(char) * (total_len + 1));
-	if (result == NULL)
-		return (NULL);
-
-	i = 0;
-	while ((s1[i] != '\0'))
-	{
-		result[i] = s1[i];
-		i++;
-	}
-
-	i = 0;
-	while (i < n && s2[i] != '\0')
-	{
-		result[len1 + i] = s2[i];
-		i++;
-	}
-
-	result[len1 + i] = '\0';
-	return (result);
+unsigned int len2, len1, total_len;
+char *result;
+unsigned int i;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = 0;
+while (s1[len1] != '\0')
+len1++;
+len2 = 0;
+while (s2[len2] != '\0')
+len2++;
+if (n >= len2)
+total_len = len1 + len2;
+else
+total_len = len1 + n;
+result = malloc(sizeof(char) * (total_len + 1));
+if (result == NULL)
+return (NULL);
+i = 0;
+while ((s1[i] != '\0'))
+{
+result[i] = s1[i];
+i++;
+}
+i = 0;
+while (i < n && s2[i] != '\0')
+{
+result[len1 + i] = s2[i];
+i++;
+}
+result[len1 + i] = '\0';
+return (result);
 }
