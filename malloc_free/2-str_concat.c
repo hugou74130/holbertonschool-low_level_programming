6aefbdea583d2d8b5938977a2c1f631e3d759dda
Hugo <hugoramos1466@gmail.com>
2025-11-10 07:21:16 +0100

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "main.h"
+
+char *str_concat(char *s1, char *s2)
+{
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -17,4 +17,5 @@ void print_times_table(int n);
 void print_number(int n);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 #endif /* MAIN_H */
