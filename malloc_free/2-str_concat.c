c9946af5262e52e60f078b53267741b8c6316f55
Hugo <hugoramos1466@gmail.com>
2025-11-10 11:58:59 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,52 +16,44 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t len1;
-	size_t len2;
-	char *result;
-	size_t i;
+size_t len1;
+size_t len2;
+char *result;
+size_t i;
 
-	len1 = 0;
-	if (s1)
-	{
-		while (s1[len1])
-
-			len1++;
-	}
-	len2 = 0;
-	if (s2)
-	{
-		while (s2[len2])
-
-			len2++;
-	}
-
-	result = malloc(len1 + len2 + 1);
-	if (result == NULL)
-
-		return (NULL);
-
-	i = 0;
-	if (s1)
-	{
-		while (s1[i])
-		{
-			result[i] = s1[i];
-			i++;
-		}
-	}
-
-	i = 0;
-	if (s2)
-	{
-		while (s2[i])
-		{
-			result[len1 + i] = s2[i];
-			i++;
-		}
-	}
-
-	result[len1 + len2] = '\0';
-
-	return (result);
+len1 = 0;
+if (s1)
+{
+while (s1[len1])
+len1++;
+}
+len2 = 0;
+if (s2)
+{
+while (s2[len2])
+len2++;
+}
+result = malloc(len1 + len2 + 1);
+if (result == NULL)
+return (NULL);
+i = 0;
+if (s1)
+{
+while (s1[i])
+{
+result[i] = s1[i];
+i++;
+}
+}
+i = 0;
+if (s2)
+{
+while (s2[i])
+{
+result[len1 + i] = s2[i];
+i++;
+}
+}
+result[len1 + len2] = '\0';
+return (result);
 }
