a309ea66652bedc03293936b04c86ef50933575e
Hugo <hugoramos1466@gmail.com>
2025-11-10 06:50:19 +0100

    betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,11 +2,13 @@
 #include "main.h"
 
 /**
- * create_array - Crée un tableau de caractères et l'initialise avec un caractère spécifique.
+ * create_array - Crée un tableau de caractères
+ * et l'initialise avec un caractère spécifique.
  * @size: La taille du tableau à créer (unsigned int).
  * @c: Le caractère utilisé pour l'initialisation.
  *
- * Retourne: Un pointeur vers le tableau alloué, ou NULL si la taille est 0 ou si l'allocation mémoire échoue.
+ * Retourne: Un pointeur vers le tableau alloué,
+ * ou NULL si la taille est 0 ou si l'allocation mémoire échoue.
  */
 
 char *create_array(unsigned int size, char c)
