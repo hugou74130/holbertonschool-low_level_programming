dfe4ee9ca6d06841fe55dc8b8e743f8f25d98306
Hugo <hugoramos1466@gmail.com>
2025-11-10 14:51:27 +0100

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,16 @@
 #include <stdlib.h>
 #include "main.h"
+/**
+ * malloc_checked - Alloue de la mémoire et gère les erreurs
+ * @b: Nombre de bytes à allouer
+ *
+ * Description: Alloue dynamiquement b bytes de mémoire à l'aide de malloc.
+ * Si l'allocation échoue, la fonction cause la terminaison du processus
+ * avec le code de sortie 98.
+ *
+ * Return: Pointeur vers la mémoire allouée
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	void *i;
