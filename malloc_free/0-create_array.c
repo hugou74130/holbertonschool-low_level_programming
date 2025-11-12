f5324cb764f0b4932f30dd45c97dcf8f5f884142
hugou74130 <132284948+hugou74130@users.noreply.github.com>
2025-11-12 07:41:21 +0100

    Update 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,23 +11,23 @@
  * or if memory allocation fails
  */
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c) // size taille du tableau, c caractère utilisé pour initialiser mon tableau
 {
-	char *arr;
-	unsigned int i;
+	char *arr; // pointeur vers le tableau 
+	unsigned int i; // compteur pour la boucle 
 
-	if (size == 0)
+	if (size == 0) // verification que size est pas = 0
 	{
 		return (NULL);
 	}
-	arr = malloc(size * sizeof(char));
-	if (arr == NULL)
+	arr = malloc(size * sizeof(char)); // allocation de la mémoire pour size
+	if (arr == NULL) // vérification apres la fin de lallocation mémoire
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)//boucle pour permettre de parcourir size
 	{
-		arr[i] = c;
+		arr[i] = c;//c permet de mettre tout les caractère initialiser dans arr 
 	}
 	return (arr);
 }
