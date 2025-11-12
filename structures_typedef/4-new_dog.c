e96b5c5ea7b59cf7ceac3c9b59f697da98aa998e
Hugo <hugoramos1466@gmail.com>
2025-11-12 10:21:59 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,53 +19,46 @@ using free_dog() or manual memory management.
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	unsigned int i;
-	int len;
+dog_t *d;
+unsigned int i;
+int len;
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+	return (NULL);
+len = 0;
+while (name[len])
+++len;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
-
-	len = 0;
-
-	while (name[len])
-		++len;
-
-	d->name = malloc(len + 1);
-	if (d->name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-	i = 0;
-	while (name[i])
-	{
-		d->name[i] = name[i];
-		i++;
-	}
-	d->name[i] = ('\0');
-
-	len = 0;
-	while (owner[len])
-		len++;
-
-	d->owner = malloc(len + 1);
-	if (d->owner == NULL)
-
-	{
-		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	i = 0;
-	while (owner[i])
-	{
-		d->owner[i] = owner[i];
-		i++;
-	}
-	d->owner[i] = ('\0');
-
-	d->age = age;
-	return (d);
+d->name = malloc(len + 1);
+if (d->name == NULL)
+{		
+free(d);
+return (NULL);
+}
+i = 0;
+while (name[i])
+{
+d->name[i] = name[i];
+i++;
+}
+d->name[i] = ('\0');
+len = 0;
+while (owner[len])
+len++;
+d->owner = malloc(len + 1);
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+i = 0;
+while (owner[i])
+{
+d->owner[i] = owner[i];
+i++;
+}
+d->owner[i] = ('\0');
+d->age = age;
+return (d);
 }
