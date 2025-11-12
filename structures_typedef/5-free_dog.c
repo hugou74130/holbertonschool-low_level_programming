888ed12164ddd349046c37c7e660d41d60f02bcd
Hugo <hugoramos1466@gmail.com>
2025-11-12 11:49:01 +0100

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,7 +1,18 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - Libère la mémoire d'une structure dog_t
+ * @d: Pointeur vers la structure dog_t
+ *
+ * Description: Désalloue complètement une structure dog_t en libérant
+ * les chaînes name et owner, puis la structure elle-même.
+ *
+ * Return: Aucune (void)
+ */
+
 void free_dog(dog_t *d)
+
 {
 	if (d)
 	{
