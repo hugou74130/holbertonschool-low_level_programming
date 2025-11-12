6e974b2acaa9859a24210749e28d8d4c18dbf25b
hugou74130 <132284948+hugou74130@users.noreply.github.com>
2025-11-12 09:11:51 +0100

    Update 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,18 +13,18 @@
 
 char *_strdup(char *str) // str est la chaine à dupliquer
 {
-	char *dup; // dup sert a 
-	unsigned int i, len; // i sert a boucler, len est pour la longueur de la memoire
+	char *dup; // dup est un pointeur vers la nouvelle chaine a dupliquée
+	unsigned int i, len; // i sert a boucler, len est pour la longueur de la chaine
 
 	if (str == NULL) // verification  de str 
 		return (NULL);// sinon return NULL
 	len = 0; // initialiser len a 0
 	while (str[len])// faire une boucle et compter avec len 
 		len++; // avancer len dans le tableau 
-	dup = malloc((len + 1) * sizeof(char)); // 
-	if (dup == NULL)
-		return (NULL);
-	for (i = 0; i <= len; i++)
-		dup[i] = str[i];
-	return (dup);
+	dup = malloc((len + 1) * sizeof(char)); // alloue de la memoire pour len + 1 de la taille d'un char 
+	if (dup == NULL)// verification de dupp 
+		return (NULL); // return NULL
+	for (i = 0; i <= len; i++) // avancement grace a i 
+		dup[i] = str[i]; //copie tout ce qui ya dans str[i] est le mets dans dup[i]
+	return (dup);// return dupp
 }
