d88730603021b3ebec9b3bb2a20e70c01a8c24fb
Hugo <hugoramos1466@gmail.com>
2025-11-17 12:27:50 +0100

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,9 +9,14 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}};
-	int i;
-	i = 0;
-	while (ops[i].op != NULL && strcmp(ops[i].op, s) != 0)
+
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
 		i++;
-	return (ops[i].f);
+	}
+	return NULL;
 }
