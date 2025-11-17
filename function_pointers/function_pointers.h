37e685728c61efa8e98a6c0021a054b2fb478772
Hugo <hugoramos1466@gmail.com>
2025-11-17 07:09:49 +0100

    1-array_iterator.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -25,4 +25,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void print_name(char *name, void (*f)(char *));
-#endif /* MAIN_H */
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif /* FUNCTION_POINTERS_H */
