50bee16d1cba3a3a643e22302153f74f144cbb74
Hugo <hugoramos1466@gmail.com>
2025-11-17 12:03:28 +0100

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+int op_add(int a, int b)
+{
+	(a) + (b);
+	return;
+}
+
+int op_sub(int a, int b)
+{
+	(a) - (b);
+	return;
+}
+
+int op_mul(int a, int b)
+{
+	(a) * (b);
+	return;
+}
+
+int op_div(int a, int b)
+{
+	(a) / (b);
+	return;
+}
+
+int op_mod(int a, int b)
+{
+	(a) % (b);
+	return;
+}
