9e7239fc14460f2462a4a4e5a3bb2a2632719ab0
Hugo <hugoramos1466@gmail.com>
2025-11-18 06:57:45 +0100

    1-print_numbers.c

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,33 @@
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_number(int n);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int sum_them_all(const unsigned int n, ...);
+#endif /* VARIADIC_FUNCTION_H */
