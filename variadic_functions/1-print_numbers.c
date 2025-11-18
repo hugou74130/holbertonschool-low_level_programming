b924d58a4e94cac7e8d1b208c275acbef038988e
Hugo <hugoramos1466@gmail.com>
2025-11-18 10:06:30 +0100

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * print_numbers - Prints numbers separated by a given separator
+ * @separator: The string to use as separator between numbers
+ * @n: The number of integers passed to the function
+ *
+ * Return: void
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 
 {
