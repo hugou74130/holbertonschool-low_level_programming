1dc2f20660e211d6f811df697e5121ed9421b726
Hugo <hugoramos1466@gmail.com>
2025-11-18 09:43:28 +0100

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (n == 0)
 
-		return (0);
+		return;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
@@ -20,5 +20,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	printf("%d, %s", n, separator);
 	va_end(args);
 
-	return (result);
+	return;
 }
