689964fa7d87bf7be61c1d3acddb025dde3340ad
Hugo <hugoramos1466@gmail.com>
2025-11-18 06:59:28 +0100

    1-print_numbers.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -18,12 +18,13 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-		;
-	return (0);
+
+		return (0);
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int),
-			va_end(args);
+		sum += va_arg(args, int);
+	va_end(args);
 	return (sum);
 }
