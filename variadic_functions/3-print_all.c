67cb11a7ec40813cfe1171c6107ff50e2224c406
Hugo <hugoramos1466@gmail.com>
2025-11-19 10:20:28 +0100

    update all code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -18,19 +18,19 @@ void print_all(const char *const format, ...)
 		switch (c)
 		{
 		case 'c':
-			printf("%c, ", va_arg(args, int));
+			printf("%c", va_arg(args, int));
 			break;
 		case 'i':
-			printf("%d, ", va_arg(args, int));
+			printf("%d", va_arg(args, int));
 			break;
 		case 'f':
-			printf("%f, ", va_arg(args, double));
+			printf("%f", va_arg(args, double));
 			break;
 		case 's':
 			s = va_arg(args, char *);
 			if (s == NULL)
 				s = "(nil)";
-			printf("%s,", s);
+			printf("%s", s);
 			break;
 		}
 		i++;
