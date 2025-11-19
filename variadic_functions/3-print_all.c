4de36b318ac0c242a76c603906dcbe9dae6c80eb
Hugo <hugoramos1466@gmail.com>
2025-11-19 10:18:53 +0100

    update all code

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char *const format, ...)
 			break;
 		}
 		i++;
-		if (format[i] && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') != '\0')
+		if (format[i] && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') == '\0')
 			printf("\b");
 	}
 	va_end(args);
