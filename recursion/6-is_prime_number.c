cb85fb63f11a537152ceb2a9bf3993c74c4f5194
Hugo <hugoramos1466@gmail.com>
2025-11-21 14:14:19 +0100

    update all code

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,11 +10,14 @@
  */
 int is_prime_number(int n, int i)
 {
-	if (n < 2)
-		return (0);
-	if (i * i > n)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	return (is_prime_number(n, i + 1));
+	if (n < 2)		// Si n est inférieur à 2…
+		return (0); // …alors ce n'est PAS un nombre premier.
+
+	if (i * i > n)	// Si i² dépasse n…
+		return (1); // …alors aucun diviseur n'a été trouvé → n est premier.
+
+	if (n % i == 0) // Si n est divisible par i…
+		return (0); // …alors n n'est PAS premier.
+
+	return (is_prime_number(n, i + 1)); // Sinon on teste le diviseur suivant (i + 1).
 }
