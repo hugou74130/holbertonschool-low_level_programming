c5dacbaf3495b405a8afb3de0e3d6cbe526768bc
Hugo <hugoramos1466@gmail.com>
2025-11-21 10:04:04 +0100

    update all code

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,22 +9,21 @@
  */
 int main(void)
 {
-int n;
-int last_digit;
+	int n;			// variable qui contiendra un nombre aleatoire
+	int last_digit; // varaible qui contiendra le last digit
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-last_digit = n % 10;
+	srand(time(0));			   // génére des nombres aléatoire different apres chaque exécution
+	n = rand() - RAND_MAX / 2; // génére un nombre aléatoire positif ou negatif qui sera decaler de - rand_max / 2
+	last_digit = n % 10;	   // prend le derniers digit de n
 
-printf("Last digit of %d is %d ", n, last_digit);
+	printf("Last digit of %d is %d ", n, last_digit); // imprime le last digit
 
-if (last_digit > 5)
-printf("and is greater than 5\n");
-else if (last_digit == 0)
-printf("and is 0\n");
-else
-printf("and is less than 6 and not 0\n");
+	if (last_digit > 5)					   // si last digit et supérieur a 5
+		printf("and is greater than 5\n"); // il affiche quil est plus grand
+	else if (last_digit == 0)			   // sinon il affiche 0
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0\n");
 
-return (0);
+	return (0);
 }
-
