b4f9d038c67be96a21c3c041b68d3b3e97f1d121
Hugo <hugoramos1466@gmail.com>
2025-12-01 07:14:11 +0100

    0-print_list.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,32 +8,35 @@
  */
 
 int main(int argc, char *argv[])
-
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+	int num1, num2, result;	  // déclare les variables num1, num2 et result
+	int (*op_func)(int, int); // déclare un pointeur vers une fonction prenant 2 int
 
-	if (argc != 4)
+	if (argc != 4) // vérifie que argc est bien égal à 4
 	{
-		printf("Error\n");
-		exit(98);
+		printf("Error\n"); // affiche "Error" suivi d'un retour à la ligne
+		exit(98);		   // quitte le programme avec le code 98
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL)
+	num1 = atoi(argv[1]);			// convertit argv[1] en entier et le stocke dans num1
+	num2 = atoi(argv[3]);			// convertit argv[3] en entier et le stocke dans num2
+	op_func = get_op_func(argv[2]); // récupère la fonction correspondant à l'opérateur donné
 
+	if (op_func == NULL) // vérifie si l'opérateur n'est pas valide
 	{
-		printf("Error\n");
-		exit(99);
+		printf("Error\n"); // affiche "Error"
+		exit(99);		   // quitte avec le code 99
 	}
+
+	// vérifie si on tente une division ou un modulo par 0
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
-		printf("Error\n");
-		exit(100);
+		printf("Error\n"); // affiche "Error"
+		exit(100);		   // quitte avec le code 100
 	}
-	result = op_func(num1, num2);
-	printf("%d\n", result);
-	return (0);
+
+	result = op_func(num1, num2); // appelle la fonction d’opération avec num1 et num2
+	printf("%d\n", result);		  // affiche le résultat suivi d’un retour à la ligne
+
+	return (0); // indique que le programme s’est exécuté correctement
 }
