3d49dded02347c052b3c40eb15a62de691a79552
Hugo <hugoramos1466@gmail.com>
2025-12-01 14:51:39 +0100

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,9 +11,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 	while (head != NULL)
-
+	{
 		temp = head->next;
-	free(head->str);
-	free(head);
-	head = temp;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
