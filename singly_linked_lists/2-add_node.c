dbb3fb1ea3c2c5bde58fea3400101346d3b3fc17
Hugo <hugoramos1466@gmail.com>
2025-12-01 07:35:02 +0100

    debug

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -20,12 +20,12 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
-		;
-	return (NULL);
+
+		return (NULL);
 
 	new->str = strdup(str);
 	if (new->str == NULL)
-		;
+
 	{
 		free(new);
 		return (NULL);
