e2c1b117dd114a7f936c4c88affa2ac4c4c52ca9
Hugo <hugoramos1466@gmail.com>
2025-12-02 14:05:34 +0100

    3-add_dnodeint_end.c

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Ajoute un nouveau nœud à la fin d'une liste doublement chaînée
+ * @head: pointeur sur le pointeur du premier nœud
+ * @n: valeur entière à ajouter
+ *
+ * Return: adresse du nouveau nœud, ou NULL en cas d'erreur
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *current;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+
+		new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new_node;
+	new_node->prev = current;
+	return (new_node);
+}
