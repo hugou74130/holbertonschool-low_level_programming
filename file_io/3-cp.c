df8ad6eaef2f354f82d15ac63174ab1d45334ec3
Hugo <hugoramos1466@gmail.com>
2025-12-03 14:44:00 +0100

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * main - Copies the content of file_from to file_to
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Description:
+ * This program copies the content of one file to another file.
+ * It reads 1024 bytes at a time from the source file and writes
+ * to the destination file. The destination file is truncated if
+ * it already exists, and created with permissions rw-rw-r-- if new.
+ *
+ * Usage: ./cp file_from file_to
+ *
+ * Exit codes:
+ * - 97: Incorrect number of arguments
+ * - 98: Cannot read from source file
+ * - 99: Cannot write to destination file
+ * - 100: Cannot close file descriptor
+ * - 0: Success
+ *
+ * Return: 0 on success, appropriate error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	/* Check if correct number of arguments */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(EXIT_USAGE);
+	}
+
+	/* Open source file for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(EXIT_READ_ERROR);
+	}
+
+	/* Open destination file for writing, create if doesn't exist, truncate if exists */
+	/* O_WRONLY: Write only */
+	/* O_CREAT: Create file if it doesn't exist */
+	/* O_TRUNC: Truncate file if it exists */
+	/* 0664: rw-rw-r-- permissions */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(EXIT_WRITE_ERROR);
+	}
+
+	/* Read and write in chunks of 1024 bytes */
+	while (1)
+	{
+		/* Read from source file */
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+
+		/* Check if read failed */
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(EXIT_READ_ERROR);
+		}
+
+		/* Break if end of file reached */
+		if (bytes_read == 0)
+			break;
+
+		/* Write to destination file */
+		bytes_written = write(fd_to, buffer, bytes_read);
+
+		/* Check if write failed or incomplete */
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(EXIT_WRITE_ERROR);
+		}
+	}
+
+	/* Close source file */
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(EXIT_CLOSE_ERROR);
+	}
+
+	/* Close destination file */
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(EXIT_CLOSE_ERROR);
+	}
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -6,8 +6,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/* Exit codes */
+#define EXIT_USAGE 97
+#define EXIT_READ_ERROR 98
+#define EXIT_WRITE_ERROR 99
+#define EXIT_CLOSE_ERROR 100
+
+/* Buffer size for file operations */
+#define BUFFER_SIZE 1024
+
+/* Function declarations */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 #endif
