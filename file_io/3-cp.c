4f06ec8945ef07f283bdfa23cc55cd27aefe0f56
Hugo <hugoramos1466@gmail.com>
2025-12-04 09:51:07 +0100

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <stdio.h>	// Pour dprintf()
+#include <stdlib.h> // Pour exit()
+#include <fcntl.h>	// Pour open(), O_RDONLY, O_WRONLY, etc.
+#include <unistd.h> // Pour read(), write(), close()
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 1024 // Taille du buffer utilisé pour copier les données
 
 /**
  * close_fd - closes a file descriptor with error handling
@@ -11,10 +11,10 @@
  */
 void close_fd(int fd)
 {
-	if (close(fd) == -1)
+	if (close(fd) == -1) // Si close échoue
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd); // Affiche une erreur sur stderr
+		exit(100);												  // Quitte le programme avec le code d'erreur 100
 	}
 }
 
@@ -26,12 +26,12 @@ void close_fd(int fd)
  */
 void error_read(int fd_from, int fd_to, char *filename)
 {
-	if (fd_from != -1)
+	if (fd_from != -1) // Si fd_from est ouvert, on le ferme
 		close_fd(fd_from);
-	if (fd_to != -1)
+	if (fd_to != -1) // Si fd_to est ouvert, on le ferme
 		close_fd(fd_to);
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	exit(98);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename); // Affiche l'erreur
+	exit(98);															  // Code d'erreur pour un problème de lecture
 }
 
 /**
@@ -42,12 +42,12 @@ void error_read(int fd_from, int fd_to, char *filename)
  */
 void error_write(int fd_from, int fd_to, char *filename)
 {
-	if (fd_from != -1)
+	if (fd_from != -1) // Ferme le fichier source si ouvert
 		close_fd(fd_from);
-	if (fd_to != -1)
+	if (fd_to != -1) // Ferme le fichier destination si ouvert
 		close_fd(fd_to);
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename); // Message d'erreur
+	exit(99);														// Code d'erreur pour un problème d’écriture
 }
 
 /**
@@ -59,30 +59,44 @@ void error_write(int fd_from, int fd_to, char *filename)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;				   // Descripteurs de fichier source et destination
+	ssize_t bytes_read, bytes_written; // Nombre d'octets lus puis écrits
+	char buffer[BUFFER_SIZE];		   // Buffer pour stocker temporairement les données
 
-	if (argc != 3)
+	if (argc != 3) // Vérifie que l'utilisateur a bien fourni 2 arguments (source et destination)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		exit(97); // Sort avec le code d'erreur 97 (mauvais usage)
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_read(-1, -1, argv[1]);
+
+	fd_from = open(argv[1], O_RDONLY); // Ouvre le fichier source en lecture seule
+	if (fd_from == -1)				   // Échec de l'ouverture
+		error_read(-1, -1, argv[1]);   // Appelle la fonction error_read
+
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	// O_WRONLY : ouvrir en écriture
+	// O_CREAT  : créer le fichier s’il n’existe pas
+	// O_TRUNC  : remplacer le contenu existant
+	// 0664     : permissions rw-rw-r--
+
+	if (fd_to == -1) // Si l'ouverture du fichier destination échoue
 		error_write(fd_from, -1, argv[2]);
+
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	// Lit jusqu'à BUFFER_SIZE octets dans le fichier source
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		// Écrit exactement le nombre d'octets lus
+
+		if (bytes_written != bytes_read) // Si tous les octets n'ont pas été écrits
 			error_write(fd_from, fd_to, argv[2]);
 	}
-	if (bytes_read == -1)
+
+	if (bytes_read == -1) // Si read a retourné une erreur
 		error_read(fd_from, fd_to, argv[1]);
-	close_fd(fd_from);
-	close_fd(fd_to);
-	return (0);
+
+	close_fd(fd_from); // Ferme proprement le fichier source
+	close_fd(fd_to);   // Ferme proprement le fichier destination
+
+	return (0); // Succès
 }
